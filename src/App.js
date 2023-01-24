42d873d6ba58d4b7f808e88193ae3f6581db2335
Jahagiresh <jahangiresh@code.edu.az>
2023-01-24 02:18:59 -0800

    router dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "./assets/css/app.scss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Services from "./pages/Services";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <Services />
-      <Footer />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
